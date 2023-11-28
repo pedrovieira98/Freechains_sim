5654ab33d7f7a20f28597ff7e63407595bb2898a
pedrovieira98 <pedrovieirama@gmail.com>
2023-11-28 10:33:02 -0300

    Updates on simul.sh and Readme

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Freechains_sim
-Repositório criado para a simulação do Freechains na disciplina de Redes P2P do Programa de Mestrado em Engenharia Eletrônica da UERJ (PEL-UERJ).
+Repositório criado para a simulação de um Fórum utilizando o protocolo **Freechains** na disciplina de Redes P2P do Programa de Mestrado em Engenharia Eletrônica da UERJ (PEL-UERJ).
 
 ## Como executar
 
-Baixe o arquivo simul.sh e execute-o em seu terminal
+Baixe o arquivo simul.sh e execute-o em seu terminal.
 
-`./simul.sh`
+```
+./simul.sh
+```
+
 
 ## Intenção da Aplicação
 
@@ -22,6 +25,18 @@ A intenção é a de simular um fórum de séries, onde os usuários irão inter
 **Troll**: Usuário com o intuito de que o fórum não funcione de maneira adequada.
 
 **Newbie**: Usuário novo e com pouca atividade, ainda aprendendo sobre os assuntos.
+
+## Usuários do Fórum por nó
+
+**Nó A**: Supondo uma casa, onde *Pioneiro* e *Ativo 1* são os moradores e participam deste mesmo fórum.
+
+**Nó B**: Usuários *Ativo 2* e *Newbie*, fazem parte deste mesmo nó, numa situação similar à do Nó A.
+
+**Nó C**: Nó no qual o usuário malicioso, *Troll*, está presente, totalmente apartado dos outros, de modo a simular que sua identidade seja completamente desconhecido do restante do fórum.
+
+## Entradas no Fórum
+
+O usuário *Pioneiro* cria o fórum e compartilha com *Ativo 1* e então *Pioneiro* decide divulgar publicamente as chaves de acesso ao fórum, permitindo a entrada dos usuários *Ativo 2*, *Newbie* e *Troll*.
 
 ## Reação dos Autores
 
--- simul.sh
+++ simul.sh
@@ -5,7 +5,6 @@
 time = $freechains-host now
 
 #Nó A
-
 A = "8330"
 Caminho_A = "/tmp/var"
 freechains-host --port="$A" start "$Caminho_A"
@@ -21,31 +20,32 @@ Caminho_C = "/tmp/var"
 freechains-host --port="$C" start "$Caminho_C"
 
 #Criação do Pioneiro
-
 pioneiro_chave = $(freechains --port="$A" crypto pubpvt 'chave-do-pioneiro')
-pub_key=$(echo $pioneiro_chave | cut -d' ' -f1);
-pri_key=$(echo $pioneiro_chave | cut -d' ' -f2);
+pioneiro_pub_key=$(echo $pioneiro_chave | cut -d' ' -f1);
+pioneiro_pri_key=$(echo $pioneiro_chave | cut -d' ' -f2);
 
 #Criação do Ativo 1
-
 ativo1_chave = $(freechains --port="$A" crypto pubpvt 'chave-do-ativo1')
-pub_key=$(echo $ativo1_chave | cut -d' ' -f1);
-pri_key=$(echo $ativo1_chave | cut -d' ' -f2);
+ativo1_pub_key=$(echo $ativo1_chave | cut -d' ' -f1);
+ativo1_pri_key=$(echo $ativo1_chave | cut -d' ' -f2);
 
 #Criação do Ativo 2
-
 ativo2_chave = $(freechains --port="$B" crypto pubpvt 'chave-do-ativo2')
-pub_key=$(echo $ativo2_chave | cut -d' ' -f1);
-pri_key=$(echo $ativo2_chave | cut -d' ' -f2);
+ativo2_pub_key=$(echo $ativo2_chave | cut -d' ' -f1);
+ativo2_pri_key=$(echo $ativo2_chave | cut -d' ' -f2);
 
 #Criação do Troll
-
 troll_chave = $(freechains --port="$C" crypto pubpvt 'chave-do-troll')
-pub_key=$(echo $troll_chave | cut -d' ' -f1);
-pri_key=$(echo $troll_chave | cut -d' ' -f2);
+troll_pub_key=$(echo $troll_chave | cut -d' ' -f1);
+troll_pri_key=$(echo $troll_chave | cut -d' ' -f2);
 
 #Criação do Newbie
-
 newbie_chave = $(freechains --port="$B" crypto pubpvt 'chave-do-newbie')
-pub_key=$(echo $newbie_chave | cut -d' ' -f1);
-pri_key=$(echo $newbie_chave | cut -d' ' -f2);
+newbie_pub_key=$(echo $newbie_chave | cut -d' ' -f1);
+newbie_pri_key=$(echo $newbie_chave | cut -d' ' -f2);
+
+#Criação do fórum
+freechains --port="$A" chains join '#séries' "$pioneiro_pub_key"
+
+#Primeiro post pelo Pioneiro explicando ideia do Fórum
+freechains --port="$A" --sign="$pioneiro_pri_key" chain '#séries' post inline "Este é um fórum para conversas e indicações de séries"
