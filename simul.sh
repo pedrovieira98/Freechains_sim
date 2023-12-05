eb23dd848a48cebd35f692512174089e1adecce2
root <root@LAPTOP-RDG254GD>
2023-12-05 09:53:50 -0300

    Update on shell file

--- simul.sh
+++ simul.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
 #Pega tempo atual
-
 time = $freechains-host now
+echo $time
+
+#Função para obter reputação
+Reputation(){
+    echo "Reputação do Pioneiro"
+    echo "freechains --host=localhost:"$A" chain '#séries' reps $pioneiro_pub_key"
+
+    echo "Reputação do Ativo1"
+    echo "freechains --host=localhost:"$A" chain '#séries' reps $ativo1_pub_key"
+
+    echo "Reputação do Ativo2"
+    echo "freechains --host=localhost:"$B" chain '#séries' reps $ativo2_pub_key"
+
+    echo "Reputação do Troll"
+    echo "freechains --host=localhost:"$C" chain '#séries' reps $troll_pub_key"
+
+    echo "Reputação do Newbie"
+    echo "freechains --host=localhost:"$B" chain '#séries' reps $newbie_pub_key"
+}
+
 
 #Nó A
 A = "8330"
@@ -44,8 +63,60 @@ newbie_chave = $(freechains --port="$B" crypto pubpvt 'chave-do-newbie')
 newbie_pub_key=$(echo $newbie_chave | cut -d' ' -f1);
 newbie_pri_key=$(echo $newbie_chave | cut -d' ' -f2);
 
+# Dia 1 -- Criação do fórum e primeiro post pelo pioneiro --
+
 #Criação do fórum
 freechains --port="$A" chains join '#séries' "$pioneiro_pub_key"
 
 #Primeiro post pelo Pioneiro explicando ideia do Fórum
-freechains --port="$A" --sign="$pioneiro_pri_key" chain '#séries' post inline "Este é um fórum para conversas e indicações de séries"
+pioneiro_post1 = $(freechains --port="$A" chain '#séries' post inline "Este é um fórum para conversas e indicações de séries" --sign="$pioneiro_pri_key")
+
+
+
+# Dia 5 -- Entrada dos membros Ativos no fórum --
+
+#Ativo1 se junta e decide postar (está no mesmo nó que o pioneiro)
+ativo1_post1 = $(freechains --port="$A" chain '#séries' post inline "Gosto muito de séries" --sign="$ativo1_pri_key")
+
+
+#Ativo2 e Newbie se juntam ao fórum de séries
+freechains --port="$B" chains join '#séries' "$pioneiro_pub_key"
+freechains --host=localhost:"$A" peer localhost:"$B" recv '#séries'
+
+#Pioneiro dá like no post de Ativo1
+freechains --port="$A" chain '#séries' --sign="$pioneiro_pri_key" like "$ativo1_post1"
+
+
+# Dia 10 -- Ativo2 faz um post e Ativo1 faz outro post --
+
+ativo2_post1 = $(freechains --port="$B" chain '#séries' post inline "Minha série favorita é The Walking Dead" --sign="$ativo2_pri_key")
+ativo1_post2 = $(freechains --port="$A" chain '#séries' post inline "Gosto de séries de fantasia" --sign="$ativo1_pri_key")
+
+#Dia 15 -- Pioneiro da like no post 2 de Ativo1 --
+
+freechains --port="$A" chain '#séries' --sign="$pioneiro_pri_key" like "$ativo1_post2"
+
+# Dia 20 -- Ativo1 e pioneiro dão like no post de Ativo2 --
+
+freechains --port="$A" chain '#séries' --sign="$pioneiro_pri_key" like "$ativo2_post1"
+freechains --port="$A" chain '#séries' --sign="$ativo1_pri_key" like "$ativo2_post1"
+
+
+# Dia 30 -- Entrada do troll no fórum e like de Ativo2 no post de Ativo1 -- 
+
+freechains --host=localhost:"$A" peer localhost:"$C" recv '#séries'
+freechains --port="$B" chain '#séries' --sign="$ativo2_pri_key" like "$ativo1_post2"
+
+# Dia 40 -- Post do troll --
+
+troll_post1 = $(freechains --port="$C" chain '#séries' post inline "Filmes são muito mais legais" --sign="$troll_pri_key")
+
+# Dia 50 -- Dislikes no post do troll
+
+freechains --port="$A" chain '#séries' --sign="$pioneiro_pri_key" dislike "$troll_post1"
+freechains --port="$A" chain '#séries' --sign="$ativo1_pri_key" dislike "$troll_post1"
+freechains --port="$B" chain '#séries' --sign="$ativo2_pri_key" dislike "$troll_post1"
+
+# Dia 90 -- Verificação de reputação
+
+Reputation
